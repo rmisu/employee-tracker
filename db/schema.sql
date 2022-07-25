00db37b3a8a282cceb63e54cb6719dae02a4e9a9
rmisu <roger889@umn.edu>
2022-07-24 22:27:56 -0500

    finally got it to run application

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,26 @@
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id),
+    roles_id INT,
+    CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL,
     manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -5,7 +5,94 @@ const consoleTable = require('console.table');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database: '',
+    password: 'abc',
+    database: 'team',
     }
 );
+
+const options = () => {
+    inquirer.prompt({
+        type: 'list',
+        name: 'choices',
+        message: 'What would you like to?',
+        choices: [
+            'View all employees',
+            'Add employee',
+            'Update employee role',
+            'View all roles',
+            'Add role',
+            'View all departments',
+            'Add department',
+            'Finish'
+        ]
+    })
+    .then((answers) => {
+        switch (answers.choices) {
+            case 'View all employees':
+                allEmployees();
+                break;
+            case 'Add employee': 
+                break;
+            case 'Update employee role':
+                break;
+            case 'View all roles':
+                allRoles();
+                break;
+            case 'Add role':
+                break;
+            case 'View all departments':
+                allDepartments();
+                break;
+            case 'Add department':
+                break;
+            case 'Finish':
+                break;
+        }
+    });
+}
+
+const allEmployees = () => {
+    db.query(`SELECT employee.id, employee.first_name, employee.last_name, employee.roles_id,
+    CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee manager
+    RIGHT JOIN employee ON employee.manager_id = manager.id; `, 
+    (err, rows) => {
+        console.table(rows);
+    return options();
+    });
+};
+
+const allRoles = () => {
+    db.query(`SELECT roles.id, roles.title, department.name AS department, roles.salary
+    FROM roles
+    LEFT JOIN department ON roles.department_id = department.id; `, 
+    (err, rows) => {
+        console.table(rows);
+    return options();
+    });
+};
+
+const allDepartments = () => {
+    db.query(`SELECT department.*
+    FROM department; `, 
+    (err, rows) => {
+        console.table(rows);
+    return options();
+    });
+};
+
+const addEmployee = () => {
+    inquirer.prompt([
+    {
+        type: 'input',
+        name: 'firstName',
+        message: 'What is the employees first name?'
+    },
+    {
+        type: 'input',
+        name: 'lastName',
+        message: 'What is the employees last name?'
+    },
+])}
+
+options();
